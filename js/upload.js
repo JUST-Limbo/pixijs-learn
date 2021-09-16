376ff4aa229c81b8d473fe33f880e7e9cae88316
limbo <1034935762@qq.com>
2021-09-16 23:34:49 +0800

    2021年9月16日23:34:48

--- js/upload.js
+++ js/upload.js
@@ -5,14 +5,10 @@ const app = new PIXI.Application({
 })
 document.getElementById('stage').appendChild(app.view)
 
-function uploadFile() {
-	document.getElementById('previewImg').src = window.URL.createObjectURL(this.files[0])
-	transformImgElementToSprite(this.files[0])
-}
-
 function appendToStage(child) {
 	app.stage.addChild(child)
 }
+
 // 从一个普通的JavaScriptImage对象创建一个纹理
 function transformImgElementToSprite(imgFile) {
 	let imageEle = new Image()
@@ -21,7 +17,13 @@ function transformImgElementToSprite(imgFile) {
 	let base = new PIXI.BaseTexture(imageEle)
 	let texture = new PIXI.Texture(base)
 	let sprite = new PIXI.Sprite(texture)
-	appendToStage(sprite)
+	return sprite
+}
+
+function uploadFile() {
+	document.getElementById('previewImg').src = window.URL.createObjectURL(this.files[0])
+	let imgSprite = transformImgElementToSprite(this.files[0])
+	appendToStage(imgSprite)
 }
 
 document.getElementById('inputFile').addEventListener('change', uploadFile)
