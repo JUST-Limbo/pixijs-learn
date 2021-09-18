bee8ef7c0d1a1892ee069f90f02259caed0236f1
limbo <1034935762@qq.com>
2021-09-19 00:47:04 +0800

    2021年9月19日00:47:03

--- js/final0.js
+++ js/final0.js
@@ -5,7 +5,7 @@ const app = new PIXI.Application({
 app.renderer.backgroundColor = 0x99ffff
 document.getElementById('stage').appendChild(app.view)
 
-document.getElementById('inputFile').addEventListener('change', importFileToStage)
+document.getElementById('inputFile').addEventListener('change', uploadFileToStage)
 
 let containerIndex = 0
 let actionType = ''
@@ -20,36 +20,34 @@ let dragParentStartPos = {
 	y: 0
 }
 
-function importFileToStage() {
+function uploadFileToStage() {
+	let imgSprite = transformImgFileToSprite(this.files[0])
 	let container = new PIXI.Container()
 	container.name = `imgContainer${containerIndex++}`
 	container.interactive = true
-	let img = new Image()
-	img.src = window.URL.createObjectURL(this.files[0])
-	img.onload = function () {
-		let base = new PIXI.BaseTexture(img)
-		let texture = new PIXI.Texture(base)
-		let imgSprite = new PIXI.Sprite(texture)
-		imgSprite.name = 'importImg'
-		imgSprite.interactive = true
-		imgSprite.cursor = 'move'
-		imgSprite.on('pointerdown', pointerDown).on('pointermove', pointerMove).on('pointerup', pointerUp)
-		container.addChild(imgSprite)
-
-		let resizeBtn = new PIXI.Sprite.fromImage('../images/scale.png')
-		resizeBtn.name = 'resizeBtn'
-		resizeBtn.width = 30
-		resizeBtn.height = 30
-		resizeBtn.interactive = true
-		resizeBtn.visible = true
-		resizeBtn.cursor = 'se-resize'
-		resizeBtn.position.set(container.getBounds().width - resizeBtn.width, container.getBounds().height - resizeBtn.height)
-		container.addChild(resizeBtn)
-
-		console.log(container.getBounds())
-	}
 	// container.on('pointerdown', pointerDown).on('pointerup', pointerUp)
+	container.addChild(imgSprite)
 	appendToStage(container)
+}
+
+// 从一个普通的JavaScriptImage对象创建一个纹理
+function transformImgFileToSprite(imgFile) {
+	let imageEle = new Image()
+	imageEle.src = window.URL.createObjectURL(imgFile)
+  imageEle.onload=function () {
+    console.log(1)
+  }
+  console.log(imageEle.onload)
+	let base = new PIXI.BaseTexture(imageEle)
+  console.log(imageEle)
+  console.log(imageEle.onload)
+	let texture = new PIXI.Texture(base)
+	let sprite = new PIXI.Sprite(texture)
+	sprite.name = 'importImg'
+	sprite.interactive = true
+	sprite.cursor = 'move'
+	sprite.on('pointerdown', pointerDown).on('pointermove', pointerMove).on('pointerup', pointerUp)
+	return sprite
 }
 
 function pointerDown(e) {
